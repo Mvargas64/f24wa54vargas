9b7362e8b80fa8d27424c1942ee189875bffe715
Mario Vargas <113541977+Mvargas64@users.noreply.github.com>
2024-10-30 21:01:06 -0500

    Update users.js
    updated user.js

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
+let value = 0;
+let total = 1;
+
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+router.get('/users', (req, res) => {
+  value += 2;
+  total += value;
+  res.send(`Total is: ${total}`);
 });
 
 module.exports = router;
